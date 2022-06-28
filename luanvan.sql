2a3981708ad25b0008fb9fdad1827a0172632ce6
haiduong03 <64822757+haiduong03@users.noreply.github.com>
2022-06-28 23:30:08 +0700

    Add files via upload

--- luanvan.sql
+++ luanvan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2022 at 05:43 PM
+-- Generation Time: Jun 28, 2022 at 06:29 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,6 +24,65 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `chitiet_sp`
+--
+
+CREATE TABLE `chitiet_sp` (
+  `SANPHAM_ID` int(11) NOT NULL,
+  `CPU` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `RAM` int(11) DEFAULT NULL,
+  `HEDIEUHANH` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `OCUNG` varchar(50) DEFAULT NULL,
+  `ANH` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `MOTA` varchar(500) CHARACTER SET utf8 NOT NULL,
+  `GIA` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `danhmuc`
+--
+
+CREATE TABLE `danhmuc` (
+  `ID` int(11) NOT NULL,
+  `TEN` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `TRANGTHAI` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hang`
+--
+
+CREATE TABLE `hang` (
+  `ID` int(11) NOT NULL,
+  `TEN` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `TRANGTHAI` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nguoidung`
+--
+
+CREATE TABLE `nguoidung` (
+  `ID` int(11) NOT NULL,
+  `TEN` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `EMAIL` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `PASS` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `SODIENTHOAI` int(11) NOT NULL,
+  `DIACHI` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `GIOITINH` tinyint(1) NOT NULL,
+  `LOAI` tinyint(1) NOT NULL,
+  `TRANGTHAI` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `sanpham`
 --
 
@@ -40,6 +99,30 @@ CREATE TABLE `sanpham` (
 --
 
 --
+-- Indexes for table `chitiet_sp`
+--
+ALTER TABLE `chitiet_sp`
+  ADD KEY `SANPHAM` (`SANPHAM_ID`);
+
+--
+-- Indexes for table `danhmuc`
+--
+ALTER TABLE `danhmuc`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `hang`
+--
+ALTER TABLE `hang`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `nguoidung`
+--
+ALTER TABLE `nguoidung`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `sanpham`
 --
 ALTER TABLE `sanpham`
@@ -52,6 +135,24 @@ ALTER TABLE `sanpham`
 --
 
 --
+-- AUTO_INCREMENT for table `danhmuc`
+--
+ALTER TABLE `danhmuc`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hang`
+--
+ALTER TABLE `hang`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nguoidung`
+--
+ALTER TABLE `nguoidung`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `sanpham`
 --
 ALTER TABLE `sanpham`
@@ -60,6 +161,12 @@ ALTER TABLE `sanpham`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `chitiet_sp`
+--
+ALTER TABLE `chitiet_sp`
+  ADD CONSTRAINT `SANPHAM` FOREIGN KEY (`SANPHAM_ID`) REFERENCES `sanpham` (`ID`);
 
 --
 -- Constraints for table `sanpham`
